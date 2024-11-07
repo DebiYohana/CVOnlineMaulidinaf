b2e338663a9c8124bb6c1f7c9d18687f3d40723f
DebiYohana <163828029+DebiYohana@users.noreply.github.com>
2024-11-07 16:12:43 +0700

    Add files via upload

---
+++ app/components/hero.tsx
@@ -0,0 +1,22 @@
+import foto from "../foto.jpeg";
+
+function Profile() {
+    return (
+    <img src={foto.src} alt="Maulidina Fauziah Rahma H" className="fotoku" />
+    );
+    
+}
+
+export default function Hero() {
+    return (
+        <div className="container mx-auto p-2 text-center">
+          <h1 className="text-gray-400 font-bold ">CV ONLINE</h1>
+          <h2 className="text-3xl">Maulidina Fauziah Rahma Hidayah</h2>
+          <Profile />
+        <p>Saya adalah seorang mahasiswa semester 5 jurusan Komputerisasi Akuntansi. Saya anak ke 1 dari 4 bersaudara, Saya tinggal di Rancaekek Abdi Negara. Cita-cita saya ingin jadi orang yang bermanfaat bagi diri sendiri, keluarga dan orang lain, serta bisa menginspirasi orang lain.
+        </p>
+        </div>
+    );
+}
+ 
+      
---
+++ app/components/riwayatpendidikan.tsx
@@ -0,0 +1,98 @@
+function RowRiwayat(props:any) {
+    return (
+      <div className="border-2 border-yellow-200/75 rounded-lg bg-white-800 p-2 my-5">
+        <div className="container mx-auto ">
+          <div className="grid grid-cols-12 gap-1">
+            <div className="col-span-12 md:col-span-4 ">{props.jenjang}</div>
+            <div className="col-span-12 md:col-span-4 ">{props.sekolah}</div>
+            <div className="col-span-12 md:col-span-4 ">{props.tahun}</div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default function RiwayatPendidikan() {
+    return (
+        <div className="container mx-auto p-2 text-center pt-20">
+        <h2 className="text-2xl">Riwayat Pendidikan</h2>
+        <RowRiwayat jenjang="SD" sekolah="SDN Abdi Negara" tahun="2011-2016" />
+        <RowRiwayat jenjang="SMP" sekolah="SMPN 1 Rancaekek" tahun="2016-2019" />
+        <RowRiwayat jenjang="SMK" sekolah="MA Baitul Arqom Al-Islami" tahun="2019-2022" />
+        <RowRiwayat jenjang="Univeristas Ma'soem" sekolah="Prodi D3 KOmputerisasi Akuntansi" tahun="2022-Sekarang" />
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
